b9d5366d0ffe7da9004463bfb03be4a69040c09e
David jose moreno Castilla <91697524+davidjmcxl@users.noreply.github.com>
2023-11-22 09:49:43 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   base_url: 'https://backend-rdo0lxo7f-davidjmcxl.vercel.app',
   token:
-    'pk.eyJ1IjoiZGF2aWRqbWN4bCIsImEiOiJjbG9nanp5dWswM29tMnBxanV0Z3IwazNjIn0.ZSnqu4Y7Iv1eSMWFWsARKg',
+    'pk.eyJ1IjoiZGF2aWRqbWN4bCIsImEiOiJjbHA5dmh5ZGMwMTBsMmtvd25oaG1wd3c0In0.cDBpwUE18nHErHQKAFsIxg',
 };
